be09c449ab0c00d490fa6ed9deffc6e6a46429bd
JuSeRDev <juserdev@gmail.com>
2026-02-03 14:19:40 -0500

    yes

---
+++ Roadmap/13 - PRUEBAS UNITARIAS/python/juserdev.py
@@ -0,0 +1,30 @@
+'''/*
+ * EJERCICIO:
+ * Crea una función que se encargue de sumar dos números y retornar
+ * su resultado.
+ * Crea un test, utilizando las herramientas de tu lenguaje, que sea
+ * capaz de determinar si esa función se ejecuta correctamente.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un diccionario con las siguientes claves y valores:
+ * "name": "Tu nombre"
+ * "age": "Tu edad"
+ * "birth_date": "Tu fecha de nacimiento"
+ * "programming_languages": ["Listado de lenguajes de programación"]
+ * Crea dos test:
+ * - Un primero que determine que existen todos los campos.
+ * - Un segundo que determine que los datos introducidos son correctos.
+ */'''
+
+'''
+  ### ANOTACIONES ###
+
+
+'''
+
+def suma(a, b):
+  if not isinstance(a, (int, float) or not isinstance(b, (int, float))):
+    raise ValueError("Los argumentos deben ser enteros o decimales")
+  return a+b
+
+print(suma(3,2))
